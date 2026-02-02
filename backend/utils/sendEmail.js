6611f8bf6abb597a0fbf8324c5c17f040d503296
sudheeesh <sudheeshvilla5@gmail.com>
2026-02-03 00:15:13 +0530

    Try Port 465 (SSL) for Gmail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -9,8 +9,8 @@ const sendEmail = async (options) => {
 
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports. 587 is STARTTLS
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
